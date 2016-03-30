a6cdebf074d8209c92b60121645e9b4b99af8571
Austin Bolstridge <bolstridge.austin@gmail.com>
2016-03-29 22:58:16 -0500

    Initial Commit :3

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("                        __    __                  \n");
+console.log("                       / _|  / _|                 \n");
+console.log("   __      __  _   _  | |_  | |_   _   _          \n");
+console.log("   \ \ /\ / / | | | | |  _| |  _| | | | |         \n");
+console.log("    \ V  V /  | |_| | | |   | |   | |_| |         \n");
+console.log("     \_/\_/    \__,_| |_|   |_|    \__, |         \n");
+console.log("                                    __/ |  ______ \n");
+console.log("                                   |___/  |______|\n");
+console.log("\n\n This... really doesn't do anything.\n");
+console.log("But hey - I got you installed on my system, didn't I? :3\n");
